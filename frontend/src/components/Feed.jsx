1c98c5a5f202121b0fcbe40dcc3880b50cfa73b1
Kgaugelo Tladi <163626679+Roise-s@users.noreply.github.com>
2024-11-19 04:16:30 +0200

    push

---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react'
+
+
+const Feed = () => {
+  return (
+    <div>
+      Feed
+    </div>
+  )
+}
+
+export default Feed
---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Search = () => {
+  console.log('passed')
+  return (
+    <div>
+      Search
+    </div>
+  )
+}
+
+export default Search
